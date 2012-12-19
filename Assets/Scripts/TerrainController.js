69dd31d73b1f028425469fc2e3f9b2ce203f2661
Quigi B Rough <quigibrough@gmail.com>
2012-12-18 21:23:14 -0500

    better terrain splosion

--- Assets/Scripts/TerrainController.js
+++ Assets/Scripts/TerrainController.js
@@ -5,6 +5,8 @@
 @script ExecuteInEditMode()
 import System.IO;
 
+var Sphere: GameObject;
+
 class TerrainController extends MonoBehaviour
 {
 	private var THICKNESS: float = .1;
@@ -40,40 +42,52 @@ class TerrainController extends MonoBehaviour
 	{
 		if (Input.GetMouseButtonDown(0))
 		{		
+			var leftExplosionIndexesTerrain: List.<int> = new List.<int>();
+			var rightExplosionIndexesTerrain: List.<int> = new List.<int>();
+			var insideExplosionRadius: List.<int> = new List.<int>();
+			
 			var leftExplosionPointsTerrain: List.<int> = new List.<int>();
 			var rightExplosionPointsTerrain: List.<int> = new List.<int>();
-			var insideExplosionRadius: List.<int> = new List.<int>();
+			
+			Debug.Log("insideCount" + insideExplosionRadius.Count);
+			//Debug.Log("0element" + insideExplosionRadius[0]);
 			
 			var mousePosition: Vector3 = Camera.mainCamera.ScreenToWorldPoint(Input.mousePosition);
 			Debug.Log("mY" + mousePosition.y);
 			Debug.Log("mX" + mousePosition.x);
 			Debug.Log("count" + points.Count);
 			
-			var explosionRadius: float = 6.0;		
+			var explosionRadius: float = 6.0;
 
-			var leftX: float = mousePosition.x - explosionRadius;
-			var rightX: float = mousePosition.x + explosionRadius;
+			var leftExplosionRadiusX: float = mousePosition.x - explosionRadius;
+			var rightExplosionRadiusX: float = mousePosition.x + explosionRadius;
 			
 			var leftPoint: Vector2;
 			var rightPoint: Vector2;
+			var leftCirclePoint: Vector2;
+			var rightCirclePoint: Vector2;
+			
+			
+			
+			
 			
 			for (var i: int = 0; i <= points.Count - 2; i++)
 			{				
-				if (points[i].x < leftX && points[i + 1].x > leftX && points[i].y > mousePosition.y - explosionRadius)
+				if (((points[i].x < leftExplosionRadiusX && points[i + 1].x > leftExplosionRadiusX) || (points[i].x > leftExplosionRadiusX && points[i + 1].x < leftExplosionRadiusX)) && points[i].y > mousePosition.y - explosionRadius)
 				{
 					Debug.Log("1boobs");
 					Debug.Log(i + 1);
 					leftPoint = points[i];
-					leftExplosionPointsTerrain.Add(i + 1); //+1 so it lands inside the explosion radius
-					//leftExplosionPointsTerrain[leftExplosionPointsTerrain.Count - 1].x = i; //x becomes i position in array
+					leftExplosionIndexesTerrain.Add(i + 1); //+1 so it lands inside the explosion radius
+					//leftExplosionIndexesTerrain[leftExplosionIndexesTerrain.Count - 1].x = i; //x becomes i position in array
 				}
-				if (points[i].x < rightX && points[i + 1].x > rightX && points[i].y > mousePosition.y - explosionRadius)
+				if (((points[i].x < rightExplosionRadiusX && points[i + 1].x > rightExplosionRadiusX) || (points[i].x > rightExplosionRadiusX && points[i + 1].x < rightExplosionRadiusX)) && points[i].y > mousePosition.y - explosionRadius)
 				{
 					Debug.Log("2boobs");
 					Debug.Log(i);
 					rightPoint = points[i + 1];
-					rightExplosionPointsTerrain.Add(i); //+0 so it lands inside the explosion radius
-					//rightExplosionPointsTerrain[rightExplosionPointsTerrain.Count - 1].x = i; //x becomes i position in array
+					rightExplosionIndexesTerrain.Add(i); //+0 so it lands inside the explosion radius
+					//rightExplosionIndexesTerrain[rightExplosionIndexesTerrain.Count - 1].x = i; //x becomes i position in array
 				}
 				if (Mathf.Sqrt(Mathf.Pow(points[i].x - mousePosition.x, 2) + Mathf.Pow(points[i].y - mousePosition.y, 2)) <= explosionRadius)
 				{
@@ -81,37 +95,103 @@ class TerrainController extends MonoBehaviour
 					Debug.Log(i);
 					insideExplosionRadius.Add(i);
 				}
+				var x0: float = mousePosition.x - points[i].x;
+				var y0: float = mousePosition.y - points[i].y;
+				var x1: float = mousePosition.x - points[i+1].x;
+				var y1: float = mousePosition.y - points[i+1].y;
+				var r: float = explosionRadius;
+				var negativeSqrt: boolean;
+				
+				if ((Mathf.Pow(-2.0*x0*y0*y1 + Mathf.Pow(2.0*x0*y1, 2) + Mathf.Pow(2.0*x1*y0, 2) - 2.0*x1*y0*y1, 2) - 4*(Mathf.Pow(-x0, 2) + 2.0*x0*x1 - Mathf.Pow(x1, 2) - Mathf.Pow(y0, 2) + 2.0*y0*y1 - Mathf.Pow(y1, 2)*(Mathf.Pow(r, 2)*Mathf.Pow(x0, 2) - 2.0*Mathf.Pow(r, 2)*x0*x1 + Mathf.Pow(r, 2)*Mathf.Pow(x1, 2) - Mathf.Pow(x0, 2)*Mathf.Pow(y1, 2) + 2.0*x0*x1*y0*y1 - Mathf.Pow(x1, 2)*Mathf.Pow(y0,2)) ) ) < 0)
+				{
+					negativeSqrt = true;
+				}
+				var resultingX1: float = (-Mathf.Sqrt( Mathf.Pow(-2.0*x0*y0*y1 + Mathf.Pow(2.0*x0*y1, 2) + Mathf.Pow(2.0*x1*y0, 2) - 2.0*x1*y0*y1, 2) - 4*(Mathf.Pow(-x0, 2) + 2.0*x0*x1 - Mathf.Pow(x1, 2) - Mathf.Pow(y0, 2) + 2.0*y0*y1 - Mathf.Pow(y1, 2)*(Mathf.Pow(r, 2)*Mathf.Pow(x0, 2) - 2.0*Mathf.Pow(r, 2)*x0*x1 + Mathf.Pow(r, 2)*Mathf.Pow(x1, 2) - Mathf.Pow(x0, 2)*Mathf.Pow(y1, 2) + 2.0*x0*x1*y0*y1 - Mathf.Pow(x1, 2)*Mathf.Pow(y0,2)) ) ) + 2.0*x0*y0*y1 - 2.0*x0*Mathf.Pow(y1, 2) - 2.0*x1*Mathf.Pow(y0, 2) + 2.0*x1*y0*y1) / (2.0*(Mathf.Pow(-x0, 2) + 2.0*x0*x1 - Mathf.Pow(x1, 2) - Mathf.Pow(y0, 2) + 2.0*y0*y1 - Mathf.Pow(y1, 2)));
+				var resultingX2: float = (Mathf.Sqrt( Mathf.Pow(-2.0*x0*y0*y1 + Mathf.Pow(2.0*x0*y1, 2) + Mathf.Pow(2.0*x1*y0, 2) - 2.0*x1*y0*y1, 2) - 4*(Mathf.Pow(-x0, 2) + 2.0*x0*x1 - Mathf.Pow(x1, 2) - Mathf.Pow(y0, 2) + 2.0*y0*y1 - Mathf.Pow(y1, 2)*(Mathf.Pow(r, 2)*Mathf.Pow(x0, 2) - 2.0*Mathf.Pow(r, 2)*x0*x1 + Mathf.Pow(r, 2)*Mathf.Pow(x1, 2) - Mathf.Pow(x0, 2)*Mathf.Pow(y1, 2) + 2.0*x0*x1*y0*y1 - Mathf.Pow(x1, 2)*Mathf.Pow(y0,2)) ) ) + 2.0*x0*y0*y1 - 2.0*x0*Mathf.Pow(y1, 2) - 2.0*x1*Mathf.Pow(y0, 2) + 2.0*x1*y0*y1) / (2.0*(Mathf.Pow(-x0, 2) + 2.0*x0*x1 - Mathf.Pow(x1, 2) - Mathf.Pow(y0, 2) + 2.0*y0*y1 - Mathf.Pow(y1, 2)));
+				
+				var firstIsOnLineSegment: boolean;
+				var secondIsOnLineSegment: boolean;
+				var leftInBetweenSegmentIndex: int;
+				var rightInBetweenSegmentIndex: int;
+				
+				var resultingY1: float = y0 + ((y1 - y0) / (x1 - x0)) * (resultingX1 - x0);
+				var resultingY2: float = y0 + ((y1 - y0) / (x1 - x0)) * (resultingX2 - x0);
+				
+				if (resultingX1 > x0 && resultingX1 < x1 && resultingY1 > y0 && resultingY1 < y1)
+				{
+					firstIsOnLineSegment = true; //since the resulting points dont fall within the lineSegment
+				}
+				if (resultingX2 > x0 && resultingX2 < x1 && resultingY2 > y0 && resultingY2 < y1)
+				{
+					secondIsOnLineSegment = true; //since the resulting points dont fall within the lineSegment
+				}
+				if (!negativeSqrt && firstIsOnLineSegment)
+				{
+					leftInBetweenSegmentIndex = i + 1;
+				}
+				if (!negativeSqrt && secondIsOnLineSegment)
+				{
+					rightInBetweenSegmentIndex = i;
+				}
+			}
+
+			
+			
+			
+			
+			var leftYoYo: Vector3 = Vector3(0.0,0.0,0.0);
+			var rightYoYo: Vector3 = Vector3(0.0,0.0,0.0);
+			leftYoYo.z = -7;
+			rightYoYo.z = -7;
+			for (var t: int = 0; t < leftExplosionIndexesTerrain.Count; t++)
+			{
+				Debug.Log("leftSplodeIndex" + leftExplosionIndexesTerrain[t]);
+				leftYoYo = points[leftExplosionIndexesTerrain[t]];
+				var PooChainCloneHead33 = Instantiate(Sphere, leftYoYo, Quaternion.LookRotation(Vector3(Mathf.Cos(MonkeyController.angle - Mathf.PI/2),Mathf.Sin(MonkeyController.angle - Mathf.PI/2),0.0),Vector3.right));
+				PooChainCloneHead33.renderer.material.color = Color.Lerp (Color.red, Color.green, 1);
+			}
+			for (var u: int = 0; u < rightExplosionIndexesTerrain.Count; u++)
+			{
+				Debug.Log("rightSplodeIndex" + rightExplosionIndexesTerrain[u]);
+				rightYoYo = points[rightExplosionIndexesTerrain[u]];
+				var PooChainCloneHead22 = Instantiate(Sphere, rightYoYo, Quaternion.LookRotation(Vector3(Mathf.Cos(MonkeyController.angle - Mathf.PI/2),Mathf.Sin(MonkeyController.angle - Mathf.PI/2),0.0),Vector3.right));
+				PooChainCloneHead22.renderer.material.color = Color.Lerp (Color.green, Color.red, 1);
 			}
 			
-			if ((leftExplosionPointsTerrain.Count / 2.0) * 2.0 == leftExplosionPointsTerrain)
+			
+			
+			
+			
+			if (leftExplosionIndexesTerrain.Count % 2.0 == 0)
 			{
-				leftExplosionPointsTerrain = new List.<int>();
+				Debug.Log("leftEvenCount");
+				leftExplosionIndexesTerrain = new List.<int>();
 			}
-			if ((rightExplosionPointsTerrain.Count / 2.0) * 2.0 == rightExplosionPointsTerrain)
+			if (rightExplosionIndexesTerrain.Count % 2.0 == 0)
 			{
-				rightExplosionPointsTerrain = new List.<int>();
+				Debug.Log("rightEvenCount");
+				rightExplosionIndexesTerrain = new List.<int>();
 			}
 			
 			var lowestLeftPoint: int = 666666;
-			var lowestRightPoint: int = 666666;
+			var lowestRightPoint: int = 0;
 			var lowestLowestLeftPoint: int = 666666;
-			var lowestLowestRightPoint: int = 666666;
+			var lowestLowestRightPoint: int = 0;
 			
-			for (var j: int = 0; j <= leftExplosionPointsTerrain.Count - 1; j++)
+			for (var j: int = 0; j <= leftExplosionIndexesTerrain.Count - 1; j++)
 			{
-				if (leftExplosionPointsTerrain[j] < lowestLeftPoint)
+				if (leftExplosionIndexesTerrain[j] < lowestLeftPoint)
 				{
-					lowestLeftPoint = leftExplosionPointsTerrain[j];
+					lowestLeftPoint = leftExplosionIndexesTerrain[j];
 				}
 			}
-			for (var k: int = 0; k <= rightExplosionPointsTerrain.Count - 1; k++)
+			for (var k: int = 0; k <= rightExplosionIndexesTerrain.Count - 1; k++)
 			{
-				if (rightExplosionPointsTerrain[k] < lowestRightPoint)
+				if (rightExplosionIndexesTerrain[k] > lowestRightPoint)
 				{
-					lowestRightPoint = rightExplosionPointsTerrain[k];
+					lowestRightPoint = rightExplosionIndexesTerrain[k];
 				}
 			}
-			
 			for (var l: int = 0; l <= insideExplosionRadius.Count - 1; l++)
 			{ 
 //				if (insideExplosionRadius == null)
@@ -129,24 +209,57 @@ class TerrainController extends MonoBehaviour
 				}
 			}
 			
+			var beginAngle: float = Mathf.PI;
+			var endAngle: float = 2 * Mathf.PI;
+			
+			if (leftInBetweenSegmentIndex < lowestLeftPoint && firstIsOnLineSegment)
+			{
+				lowestLeftPoint = leftInBetweenSegmentIndex;
+				beginAngle = Mathf.Atan(resultingY1 / resultingX1);				
+			}
+			if (rightInBetweenSegmentIndex > lowestRightPoint && secondIsOnLineSegment)
+			{
+				lowestRightPoint = rightInBetweenSegmentIndex;
+				endAngle = Mathf.Atan(resultingY2 / resultingX2);
+			}
+			if (endAngle > 2 * Mathf.PI)
+			{
+				endAngle = 2 * Mathf.PI;
+			}
+			if (beginAngle < Mathf.PI)
+			{
+				beginAngle = Mathf.PI;
+			}						
+			
+			leftYoYo = points[lowestLeftPoint];
+			rightYoYo = points[lowestRightPoint];
+			leftYoYo.z = -7;
+			rightYoYo.z = -7;
+			//places markers at destroyed indexes, or one off of them or something
+			var PooChainCloneHead = Instantiate(Sphere, leftYoYo, Quaternion.LookRotation(Vector3(Mathf.Cos(MonkeyController.angle - Mathf.PI/2),Mathf.Sin(MonkeyController.angle - Mathf.PI/2),0.0),Vector3.right));
+			var PooChainCloneHead1 = Instantiate(Sphere, rightYoYo, Quaternion.LookRotation(Vector3(Mathf.Cos(MonkeyController.angle - Mathf.PI/2),Mathf.Sin(MonkeyController.angle - Mathf.PI/2),0.0),Vector3.right));
+			PooChainCloneHead.renderer.material.color = Color.Lerp (Color.red, Color.green, 1);
+			PooChainCloneHead1.renderer.material.color = Color.Lerp (Color.green, Color.red, 1);
+			
 			//maybe needs to not be both cause if one side is off it might not mean other side is off too
-			if (lowestLeftPoint == 666666) 
+			if (lowestLeftPoint == 666666)
 			{
+			Debug.Log("return1");
 				return;
 			}
-			if (lowestRightPoint == 666666) 
+			if (lowestRightPoint == 0)
 			{
+			Debug.Log("return2");
 				return;
 			}
-
 			Debug.Log("Removed" + lowestLeftPoint + "to" + lowestRightPoint);
 			points.RemoveRange(lowestLeftPoint, lowestRightPoint - lowestLeftPoint + 1);			
-						
+			
 			//mousePosition.y = mousePosition.y + 10.0;
 			//points.Insert(lowestLeftPoint, mousePosition);
 			
 			var numberOfSegmentsInCircleBottom: int = explosionRadius * 2 - 1;
-			var increaseAngleBy: float = Mathf.PI / numberOfSegmentsInCircleBottom;
+			var increaseAngleBy: float = (endAngle - beginAngle) / numberOfSegmentsInCircleBottom;
 			
 			var counter: int = 0;
 			
@@ -163,11 +276,11 @@ class TerrainController extends MonoBehaviour
 					points.Insert(m, leftPoint);
 				}
 								
-				if (m > lowestLeftPoint + 1 && m < lowestLeftPoint + 2 + numberOfSegmentsInCircleBottom)
+				if (m > lowestLeftPoint + 1 && m < lowestLeftPoint + 2 + numberOfSegmentsInCircleBottom && beginAngle + increaseAngleBy * counter < endAngle)
 				{
 					counter++;
-					var yCircleBottom: float = mousePosition.y + explosionRadius * Mathf.Sin(Mathf.PI + increaseAngleBy * counter);
-					var xCircleBottom: float = mousePosition.x + explosionRadius * Mathf.Cos(Mathf.PI + increaseAngleBy * counter);
+					var yCircleBottom: float = mousePosition.y + explosionRadius * Mathf.Sin(beginAngle + increaseAngleBy * counter);
+					var xCircleBottom: float = mousePosition.x + explosionRadius * Mathf.Cos(beginAngle + increaseAngleBy * counter);
 					var circleBottomPoint: Vector2 = Vector2(xCircleBottom, yCircleBottom);
 					points.Insert(m, circleBottomPoint);
 				}
@@ -183,6 +296,7 @@ class TerrainController extends MonoBehaviour
 				}
 			}
 			if(!buildTerrainMesh()){Debug.Log("terrainFailed");}
+
 		}
 	}
 	
